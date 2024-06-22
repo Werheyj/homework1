afa87ee1da7671ab5374ba1a7c3951f4777f8453
Tacha <Anastasiykoshe@gmail.com>
2024-06-22 13:41:38 +0300

    Модуль 1 задание 2

---
+++ hw2.py
@@ -0,0 +1,12 @@
+# Количество выполненных ДЗ (запишите значение 12)
+# Количество затраченных часов (запишите значение 1.5)
+# Название курса (запишите значение 'Python')
+# Время на одно задание (вычислить используя 1 и 2 переменные)
+
+
+quantity_made_hw = 12
+quantity_spent_h = 1.5
+сourse_name = 'Python'
+time_for_one_task = quantity_spent_h / quantity_made_hw
+
+print(f'Курс: {сourse_name}; Всего задач: {quantity_made_hw}; Затрачено часов: {quantity_spent_h}; Среднее время выполнения: {time_for_one_task} часа.')
