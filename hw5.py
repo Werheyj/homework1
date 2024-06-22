8809899fdc5914e9e322e6a63b995bdd42b97060
Tacha <Anastasiykoshe@gmail.com>
2024-06-22 20:06:01 +0300

    Модуль 1 задание 5

---
+++ hw5.py
@@ -0,0 +1,7 @@
+immutable_var = ('John', 35, 1.85, True)
+print(immutable_var)
+# immutable_var[1] = 29
+# кортеж - это неизменяемый объект.
+mutable_list = ['John', 35, 1.85, True]
+mutable_list[1] = 29
+print(mutable_list)
