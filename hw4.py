3293f09b9d7509a481fc2c6697758d5be2f91a87
Tacha <Anastasiykoshe@gmail.com>
2024-06-22 16:37:51 +0300

    Модуль 1 задание 4

---
+++ hw4.py
@@ -0,0 +1,14 @@
+# Выведите строку my_string в верхнем регистре.
+# Выведите строку my_string в нижнем регистре.
+# Измените строку my_string, удалив все пробелы.
+# Выведите первый символ строки my_string.
+# Выведите последний символ строки my_string.
+# my string is good
+
+my_string = input()
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
